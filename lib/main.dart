81755c263d6849fe935d49fa6dfc0efcea3890ca
Oscar Lauth <oscar.lauth@gmail.com>
2020-07-31 20:16:02 -0400

    Add to bottomsheet and themedata

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import './menu.dart';
 
 void main() => runApp(MyApp());
@@ -8,8 +9,11 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         theme: ThemeData(
-            primaryColor: new Color(0xFF324E75),
-            scaffoldBackgroundColor: new Color(0xFF0E2F4E)),
+          primaryColor: new Color(0xFF324E75),
+          backgroundColor: new Color(0xFF0E2F4E),
+          accentColor: new Color(0xFF1FFEAD),
+          hintColor: new Color(0xFF136768),
+        ),
         home: Menu());
   }
 }
--- lib/menu.dart
+++ lib/menu.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class Menu extends StatefulWidget {
@@ -7,21 +8,25 @@ class Menu extends StatefulWidget {
 }
 
 class MenuState extends State<Menu> {
+  bool _validate = true;
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          title: Text('Werewolf Menu',
-              style: GoogleFonts.medievalSharp(
-                  color: new Color(0xFF1FFEAD), fontSize: 30))),
+        title: Text('Werewolf Menu',
+            style: GoogleFonts.medievalSharp(
+                color: Theme.of(context).accentColor, fontSize: 30)),
+      ),
       body: Center(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.end,
           children: <Widget>[
             FlatButton(
                 child: Text("Join Game",
                     style: GoogleFonts.medievalSharp(
-                        color: new Color(0xFF0E2F4E), fontSize: 30)),
-                color: new Color(0xFF1FFEAD),
+                        color: Theme.of(context).backgroundColor,
+                        fontSize: 30)),
+                color: Theme.of(context).accentColor,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(50)),
                 onPressed: () {
@@ -33,11 +38,60 @@ class MenuState extends State<Menu> {
                         return Container(
                           height: MediaQuery.of(context).size.height * 0.75,
                           decoration: new BoxDecoration(
-                            color: Colors.white,
+                            color: Theme.of(context).backgroundColor,
                             borderRadius: new BorderRadius.only(
                               topLeft: const Radius.circular(25.0),
                               topRight: const Radius.circular(25.0),
                             ),
+                          ),
+                          child: Column(
+                            children: <Widget>[
+                              SizedBox(height: 15),
+                              Text(
+                                "Join Game",
+                                style: TextStyle(
+                                  fontSize: 24,
+                                  color: Theme.of(context).accentColor,
+                                ),
+                              ),
+                              Container(
+                                margin: EdgeInsets.only(
+                                    top: 60, left: 120, right: 120),
+                                child: TextField(
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                    fontSize: 40,
+                                    color: Theme.of(context).accentColor,
+                                  ),
+                                  decoration: InputDecoration(
+                                    hintText: "Game ID",
+                                    hintStyle: TextStyle(
+                                        color: Theme.of(context).hintColor),
+                                    counterText: "",
+                                    errorText:
+                                        _validate ? null : 'Invalid Game ID',
+                                  ),
+                                  inputFormatters: [
+                                    WhitelistingTextInputFormatter.digitsOnly
+                                  ],
+                                  keyboardType: TextInputType.numberWithOptions(
+                                      signed: false, decimal: false),
+                                  maxLength: 3,
+                                  maxLengthEnforced: true,
+                                  onChanged: (String inputGameID) {
+                                    if (inputGameID.length == 3) {
+                                      //_validate = isValid(inputGameID);
+                                      setState(() {
+                                        _validate = false;
+                                      });
+                                      if (_validate) {
+                                        //joinGame(inputGameID)/send to lobby page
+                                      }
+                                    }
+                                  },
+                                ),
+                              ),
+                            ],
                           ),
                         );
                       });
@@ -45,8 +99,8 @@ class MenuState extends State<Menu> {
             FlatButton(
                 child: Text("Host Game",
                     style: GoogleFonts.medievalSharp(
-                        color: new Color(0xFF1FFEAD), fontSize: 30)),
-                color: new Color(0xFF0E2F4E),
+                        color: Theme.of(context).accentColor, fontSize: 30)),
+                color: Theme.of(context).backgroundColor,
                 shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(50)),
                 onPressed: () {
@@ -58,7 +112,7 @@ class MenuState extends State<Menu> {
                         return Container(
                           height: MediaQuery.of(context).size.height * 0.75,
                           decoration: new BoxDecoration(
-                            color: Colors.white,
+                            color: Theme.of(context).backgroundColor,
                             borderRadius: new BorderRadius.only(
                               topLeft: const Radius.circular(25.0),
                               topRight: const Radius.circular(25.0),
